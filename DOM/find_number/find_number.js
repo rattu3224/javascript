36cc3830b0dd6af55fad1d88551f535dfdf7098e
Rattu1206 <61944272+rattu3224@users.noreply.github.com>
2025-12-18 14:01:43 +0530

    reusable code

--- DOM/find_number/find_number.js
+++ DOM/find_number/find_number.js
@@ -4,6 +4,12 @@ let secretNumber = Math.trunc(Math.random() * 20)+1;
 let score = 20; // state variable
 let highScore = document.querySelector('.highscore').textContent;
 
+const displayMsg = (txtMsg)=>{
+    document.querySelector('.message').textContent= txtMsg;//'Correct Number';
+}
+const bodyColor = (color)=>{
+    document.querySelector('body').style.backgroundColor = color;//'#60b347';
+}
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -15,8 +21,9 @@ document.querySelector('.check').addEventListener('click', function(){
     else if(guess === secretNumber)
     {
         // highScore ++ ;
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.message').textContent= 'Correct Number';
+        // document.querySelector('body').style.backgroundColor = '#60b347';
+        bodyColor('#60b347');
+        displayMsg('Correct Number');
         document.querySelector('.number').textContent = secretNumber;
         if(highScore == 0 || highScore < score)
         {
@@ -26,19 +33,24 @@ document.querySelector('.check').addEventListener('click', function(){
     else if(guess > secretNumber)
     {
         score -=1;
-        document.querySelector('body').style.backgroundColor = '#b37d47';
-        document.querySelector('.message').textContent= 'Too large';
+        // document.querySelector('body').style.backgroundColor = '#b37d47';
+        // document.querySelector('.message').textContent= 'Too large';
+        bodyColor('#60b347');
+        displayMsg('Too large');
     }
     else if(guess < secretNumber)
     {
         score -=1;
-        document.querySelector('.message').textContent= 'Too Small';
+        // document.querySelector('.message').textContent= 'Too Small';
+        displayMsg('Too Small');
     }
 
     if(score === 0)
     {
-        document.querySelector('body').style.backgroundColor = 'red';
-        document.querySelector('.message').textContent = 'you lost the game';
+        // document.querySelector('body').style.backgroundColor = 'red';
+        // document.querySelector('.message').textContent = 'you lost the game';
+        bodyColor('#FF0000');
+        displayMsg('😖 You lost the game');
         return;
     }
     document.querySelector('.score').textContent = score;
