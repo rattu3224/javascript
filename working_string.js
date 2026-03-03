531336d572efd1d9318d0f8a5a33a335a13398b1
rattu3224 <61944272+rattu3224@users.noreply.github.com>
2026-03-03 21:46:32 +0530

    strings exercise

--- working_string.js
+++ working_string.js
@@ -184,5 +184,17 @@ planesInLine(3)
 planesInLine(12)
 planesInLine(5)
 
+// strings methods practice
+
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// console.log(flights.split('+'));
+for(const flight of flights.split('+'))
+{
+    const [type,from,to,time] = flight.split(';');
+    const output = `${type.includes('Delayed')?'🔴':''} ${type.replaceAll('_',' ')} from ${from.replace(/\d/g,'')} to ${to.replace(/\d/g,'')} (${time.replace(':','h')})`;
+    console.log(output);
+
+}
 
 
