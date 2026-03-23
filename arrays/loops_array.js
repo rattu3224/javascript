9e9162c3b3e096bb88e4cd908adc011d4b3f4251
rattu3224 <61944272+rattu3224@users.noreply.github.com>
2026-03-23 22:07:32 +0530

    view for bakist app
    HTML
    CSS -- no xternal lib

--- arrays/loops_array.js
+++ arrays/loops_array.js
@@ -23,4 +23,22 @@ movements.forEach(function(movement,index,arr){
     // console.log(arr);// gives the whole of the array
     let action = (movement > 0)?'deposited':'withdraw';
     console.log(`Movement ${index+1}: You ${action} ${Math.abs(movement)}`);
+});
+
+
+
+// WORKING WITH MAPS & SETS
+
+currencies.forEach(function(curr,key){
+    console.log(`${key}: ${curr}`)
 })
+
+const currenciesSet = new Set(currencies);
+const currenciesUnique = new Set(['USD','GBP','USD','EUR','EUR']);
+// set is not key value pair 
+// SET doen't have index/key so it consider value as key
+// for of is best for set
+currenciesUnique.forEach(function(curr,key){
+    console.log(`${key}: ${curr}`)
+});
+
